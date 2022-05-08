38cd42a98ce56d76006f42976536ebdeb1f0cd3b
anudeeps28 <anudeepsharma28@gmail.com>
2022-05-08 14:26:40 -0400

    instance variable

---
+++ Object-Orientation-Python/OOP_Exercise_8.py
@@ -0,0 +1,19 @@
+'''
+ Determine if School_bus is also an instance of the Vehicle class
+
+'''
+
+class Vehicle: 
+    def __init__(self, name, milage, capacity):
+        self.name = name
+        self.milage = milage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_Bus = Bus("School Volvo", 12, 50)
+
+# ininstance() function built-in python
+print(isinstance(School_Bus, Vehicle)) #true because this is an instance variable of class Vehicle
+print(isinstance(Bus, Vehicle)) #false becuse bus not a variable, it is a sub-class
