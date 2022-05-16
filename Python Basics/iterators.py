f7fb3fabce13bc5865efc1e96851c03616e887e7
anudeeps28 <anudeepsharma28@gmail.com>
2022-05-16 13:09:49 -0400

    OOP iterators remote control

---
+++ Python Basics/iterators.py
@@ -0,0 +1,24 @@
+# making a class for remote control to switch to next channel 
+# just like iterators switch to next while iterating
+
+class RemoteControl():
+    def __init__(self):
+        self.channels = ["HBO", "espn", "cnn"]
+        self.index = -1
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.index += 1
+        if self.index == len(self.channels):
+            raise StopIteration
+        return self.channels[self.index]
+
+r = RemoteControl()
+itr = iter(r)
+
+print(next(itr))
+print(next(itr))
+print(next(itr))
+print(next(itr))
