4921c56a6dcee776f22555570afece948d6b5b8b
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-04 14:16:21 -0500

    stack implementation in python

---
+++ Stack/stackBasics.py
@@ -0,0 +1,19 @@
+# python list can be used as a stack
+
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+print(stack)  # Output: [1, 2, 3]
+print(stack.pop()) # Output: 3; also removes 3 from the stack
+print(stack.pop()) # Output: 2; also removes 2 from the stack
+print(stack.pop()) # Output: 1; also removes 1 from the stack
+print(stack)  # Output: []
+
+# there is no .top()/.peek() methods in python stack; 
+# use the "-1th" index access the "top" (value inserted at the last) value
+stack = [1, 2, 3]
+top_element = stack[-1]
+print(top_element)  # Output: 3
+
+
