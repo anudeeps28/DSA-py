36ae89069ff7231ed26e65a5edb5eec15352f881
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-10 14:51:53 -0500

    replace, count and f-strings

--- Python Basics/strings.py
+++ Python Basics/strings.py
@@ -11,12 +11,15 @@ print(text)
 print(text[0]) 
 print(text[1])
 
-# strings are immutable 
+# strings are immutable
 '''
 this will give error
 text[0] = 'l'
-
 '''
+# instead use replace() to make a new string with changes in the original one
+message = "hello world"
+newMessage = message.replace("world", "universe") 
+print(newMessage)
 
 # extracting sub-string in this main string
 print(text[0:3])
@@ -26,6 +29,9 @@ print(text[:3])
 # both are the same thing
 a = "bob"
 a = 'bob'
+
+# count method in strings
+print(a.count('b')) # have to give some argument inside count method
 
 # multiple line strings  = use triple-quotes
 address = '''282 Littleton Street
@@ -49,6 +55,23 @@ num = 50
 print(s+num)
 '''
 
-s = "total states in the USA are"
+# string concatination
+s = "total states in the USA are "
 num = 50
 print(s+str(num))
+
+# a better way to do string concatination
+'''
+if there are large number of strings that we need to join, using a 
++ sign can be difficult to keep track of, so we use formatted strings
+'''
+answer = '{}{}'.format(s, num)
+answer2 = '{}, {}'.format(s, num)
+print(answer)
+print(answer2)
+
+# f-strings
+answer3 = f'{s}, {num}'
+print(answer3)
+ 
+print(help(str))
