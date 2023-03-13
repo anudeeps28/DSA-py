2d8960dbef1b3168c9b27b99ec089efa3e367694
anudeeps28 <anudeepsharma28@gmail.com>
2023-03-12 23:55:23 -0400

    lambda syntax

---
+++ Python Basics/lambdaPython.py
@@ -0,0 +1,13 @@
+# normal function
+def func(x):
+    return x+5
+
+print(func(2))
+
+# using lambda
+func2 = lambda x: x+5/4
+print(func2(2))
+
+# multiple parameters
+func3 = lambda x,y: x+y
+print(func3(2,3))
