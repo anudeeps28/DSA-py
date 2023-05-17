c813c939a92e32dfc8a11353f6820ff3145bd22e
anudeeps28 <anudeepsharma28@gmail.com>
2023-05-17 19:26:17 -0400

    using map method for taking inputs

--- Python Basics/input.py
+++ Python Basics/input.py
@@ -4,3 +4,14 @@ print("Username is: " + username)
 # python 2.7 uses raw input
 # username = raw_input("Enter username:")
 # print("Username is: " + username)
+
+### METHOD - 2 ###
+
+# Read a line of space-separated integers from the user
+input_str = input("Enter a line of integers: ")
+
+# Split the input string into individual values and convert them to integers
+arr = map(int, input_str.split())
+
+# Print the resulting sequence of integers
+print(list(arr))
