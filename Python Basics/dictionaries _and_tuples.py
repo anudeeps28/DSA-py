3f7d3cf3661a153199ff35e006e2404e8873847f
anudeeps28 <anudeepsharma28@gmail.com>
2023-05-17 19:46:26 -0400

    tuples basics and methods

--- Python Basics/dictionaries _and_tuples.py
+++ Python Basics/dictionaries _and_tuples.py
@@ -1,3 +1,4 @@
+### DICTIONARY = MAPS ###
 # dictionary = telephone directory
 d = {"tom": 462239, "rob":7493272349, "joe":46294723} # making a dictionary
 
@@ -38,9 +39,24 @@ d.clear()
 
 print("///////////////////////////////////////////////////////////")
 
+### TUPLES = immutable lists ###
+
+# Creating an empty tuple
+empty_tuple = ()
+
+# Creating a tuple with elements
+my_tuple = (1, 2, 3)
+another_tuple = 4, 5, 6
+mixed_tuple = ('apple', 1, True)
+
+
 # tuples = list of values grouped together
 point = (5,9) # defining a tuple
 print(point[1])
+
+my_tuple = (1, 2, 3)
+print(my_tuple[0])  # Output: 1
+print(my_tuple[2])  # Output: 3
 
 # another example of tuple
 address = ("282 Littleton St.", "Crestview NorthApartments", 47906)
@@ -48,6 +64,33 @@ address = ("282 Littleton St.", "Crestview NorthApartments", 47906)
 for i in address:
     print(i)
 
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple[1:4])  # Output: (2, 3, 4)
+
+tuple1 = (1, 2)
+tuple2 = (3, 4)
+concatenated_tuple = tuple1 + tuple2
+print(concatenated_tuple)  # Output: (1, 2, 3, 4)
+
+repeated_tuple = (1, 2) * 3
+print(repeated_tuple)  # Output: (1, 2, 1, 2, 1, 2)
+
+my_tuple = (1, 2, 3)
+a, b, c = my_tuple
+print(a)  # Output: 1
+print(b)  # Output: 2
+print(c)  # Output: 3
+
+
+my_tuple = (1, 2, 3)
+# my_tuple[0] = 10  # This will result in an error
+
+my_tuple = (10, 2, 3)  # Create a new tuple and assign it to the same variable
+
+my_tuple = (1, 2, 2, 3, 2)
+print(my_tuple.count(2))  # Output: 3
+print(my_tuple.index(3))  # Output: 3
+
+
 # we can create a list of tuples
 list_of_tuples = [(4,5), (4,7), (7,8)]
-
